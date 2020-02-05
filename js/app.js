c5f97a108b1bfec3ea80d54fe1deea84536308a1
Joel Gainey <jwgainey@hotmail.com>
2020-02-05 16:38:27 -0500

    "spoiler" button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// hide spoiler
+$(".spoiler span").hide();
+//  show spoiler when the button is pressed
+$(".spoiler button").click( function () {
+    $(".spoiler span").slideDown();
+    $(".spoiler button").fadeOut(1000);
+});
+// hide button
+
 
 
 
