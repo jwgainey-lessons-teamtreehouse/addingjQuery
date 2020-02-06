6ec6851b6bb74b2ecc97dc3398cac3db42574dd9
Joel Gainey <jwgainey@hotmail.com>
2020-02-06 15:32:27 -0500

    event delegation and proagation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+// $(".spoiler button").click( function () {
+//     $(".spoiler span").slideDown();
+//     $(".spoiler button").fadeOut(1000);
+// });
+$('.spoiler').on('click', 'button', function() {
+    $(".spoiler span").slideDown();
+    $(".spoiler button").fadeOut(1000);
+});
+
 const $button = $("<button>RevealSpoiler</button>")
 
 $(".spoiler").append($button);
 
 $(".spoiler span").hide();
-$(".spoiler button").on('click mouseleave', function () {
-    $(".spoiler span").slideDown();
-    $(".spoiler button").fadeOut(1000);
-});
+
 
