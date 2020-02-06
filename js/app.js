3aad6b2e85506b0d9a3e5dd6473e5456e635b67a
Joel Gainey <jwgainey@hotmail.com>
2020-02-06 16:17:13 -0500

    event onject (event.target)

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 //     $(".spoiler span").slideDown();
 //     $(".spoiler button").fadeOut(1000);
 // });
-$('.spoiler').on('click', 'button', function() {
-    $(".spoiler span").slideDown();
-    $(".spoiler button").fadeOut(1000);
+$('.spoiler').on('click', 'button', function(event) {
+    console.log(event.target);
+    //$(".spoiler span").show();
+    $(event.target).hide();
 });
 
 const $button = $("<button>RevealSpoiler</button>")
