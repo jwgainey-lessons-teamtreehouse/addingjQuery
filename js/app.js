d6186e15dbca1cf69003611b1e04251db655aab5
Joel Gainey <jwgainey@hotmail.com>
2020-02-06 14:59:13 -0500

    .on() method 1

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const $button = $("<button>RevealSpoiler</button>")
 $(".spoiler").append($button);
 
 $(".spoiler span").hide();
-$(".spoiler button").click( function () {
+$(".spoiler button").on('click mouseleave', function () {
     $(".spoiler span").slideDown();
     $(".spoiler button").fadeOut(1000);
 });
